af0fca2e105b843bbe92d452356b0e0aa355f0c5
M1-05 <rvshvs@hotmail.com>
2020-12-20 20:07:46 +0300

    test1

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string character = "Ahmad";
+            int age = 15;
+            string superpower1 = "steel armor";
+            string superpower2 = "night vision";
+            double speed = 1.65;
+            Console.WriteLine("My name is {0}, my super powers are {1} and {2}, my age is {3} and my speed is {4}.", character, superpower1, superpower2, age, speed);
+        }
+    }
+}
