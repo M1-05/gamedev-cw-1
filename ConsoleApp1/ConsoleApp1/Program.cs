d757e6a33bdbcdaaea86294f334223b80c24c339
M1-05 <rvshvs@hotmail.com>
2020-12-21 17:53:51 +0300

    test2

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -12,6 +12,14 @@ namespace ConsoleApp1
             string superpower2 = "night vision";
             double speed = 1.65;
             Console.WriteLine("My name is {0}, my super powers are {1} and {2}, my age is {3} and my speed is {4}.", character, superpower1, superpower2, age, speed);
+            //second task
+            string heroName = "John";
+            string heroHeight = "193cm";
+            int heroAge = 22;
+            string heroSuperPower1 = "strong punch";
+            string heroSuperPower2 = "high jumps";
+            int ageDifference = heroAge - age;
+            Console.WriteLine("the age difference is : {0}", ageDifference);
         }
     }
 }
